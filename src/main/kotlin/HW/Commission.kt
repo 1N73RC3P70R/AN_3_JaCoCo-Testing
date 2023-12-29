e7aebb9d6e1dc9a87cf728fad1904dc56058908a
1N73RC3P70R <kras.konstantinov@yandex.ru>
2023-12-29 20:02:02 +0300

    ДЗ 2.2 - DONE

--- src/main/kotlin/HW/Commission.kt
+++ src/main/kotlin/HW/Commission.kt
@@ -1,16 +1,74 @@
-const val MINIMUM_RATE: Double = 35.0;
-const val STANDARD_RATE: Double = 0.0075;
-const val ZERO: Double = 0.0;
+const val MINIMUM_RATE: Double = 35.0
+const val STANDARD_RATE: Double = 0.0075
+const val ZERO: Double = 0.0
 const val UPPER_LIMIT_MASTERCARD_DAILY: Double = 75_000.00
 
+var day = 1
 var totalSpentMonth = 0.0
 var commissionTotal = 0.0
 var amount = 0.0
-var cardChoice = ""
+var inputCardChoice = ""
 
+fun main() {
+    commission()
+}
 
 fun commission() {
+    usersCardChoice()
 
+    val continueRunningProgram: Boolean = true
+    while (continueRunningProgram) {
+        println("Введите сумму: ")
+        amount = readLine()!!.toDoubleOrNull() ?: 0.0
+
+        if (amount <= 0 || amount > 150_000) {
+            println("Неправильная сумма.")
+            continue
+        }
+
+        totalSpentMonth += amount
+
+        when (inputCardChoice) {
+            "Visa", "Мир" -> standardCommissionCalculator()
+            "Mastercard" -> {
+                if (totalSpentMonth in ZERO..UPPER_LIMIT_MASTERCARD_DAILY) {
+                    println("Комиссия с Mastercard отсутствует.")
+                    commissionTotal = 0.0
+                } else if (totalSpentMonth > UPPER_LIMIT_MASTERCARD_DAILY) {
+                    println("${amount * STANDARD_RATE} руб. - комиссия с $inputCardChoice")
+                }
+            }
+            "VK Pay" -> {
+                commissionTotal = 0.0
+                println("Комиссия не взимается.")
+            }
+        }
+
+        dayPrompt()
+    }
+}
+
+fun dayPrompt() {
+    println(
+        """Потрачено за месяц: $totalSpentMonth
+            |Хотите ли продолжить?
+            |1. Да
+            |2. Нет (Закрыть программу)
+        """.trimMargin()
+    )
+    //Это сделал так, как будто каждая операция проводиться на следующий день.
+    day++
+    when (readLine()?.toIntOrNull()) {
+        1 -> println("Продолжаем. День $day.")
+        2 -> return
+        else -> {
+            println("Нет такого варианта.")
+            return
+        }
+    }
+}
+
+fun usersCardChoice() {
     println(
         """Выберите карту:
     | 1. Мир
@@ -19,9 +77,9 @@ fun commission() {
     | 4. Mastercard 
 """.trimMargin()
     )
-    cardChoice = readLine().toString()
+    inputCardChoice = readLine().toString()
 
-    cardChoice = when (cardChoice?.toInt()) {
+    inputCardChoice = when (inputCardChoice?.toIntOrNull()) {
         1 -> "Мир"
         2 -> "VK Pay"
         3 -> "Visa"
@@ -31,61 +89,14 @@ fun commission() {
             "VK Pay"
         }
     }
-
-
-    val continueRunningProgram: Boolean = true
-    while (continueRunningProgram) {
-        println("Введите сумму: ")
-        amount = readLine()!!.toDoubleOrNull()!!;
-
-        if (amount<= 0 || amount > 150_000) {
-            println("Неправильная сумма.")
-            continue
-        }
-
-        if (amount != null) {
-            totalSpentMonth += amount
-        } else {
-            println("Ошибка!")
-        }
-
-        when (cardChoice) {
-            "Visa", "Мир" -> standardCommissionCalculator()
-            "Mastercard" -> if (totalSpentMonth in ZERO..UPPER_LIMIT_MASTERCARD_DAILY) {
-                println("""Комиссия с Mastercard отсутствует.""")
-                commissionTotal = 0.0
-            } else if (totalSpentMonth > UPPER_LIMIT_MASTERCARD_DAILY) {
-                println("""${amount * STANDARD_RATE} руб. - комиссия с $cardChoice""")
-            }
-            "VK Pay" -> {
-                commissionTotal = 0.0;
-                println("Комиссия не взимается.")
-            }
-        }
-
-        println(
-            """Потрачено за месяц: $totalSpentMonth
-            |Хотите ли продолжить?
-            |1. Да
-            |2. Нет (Закрыть программу)
-        """.trimMargin()
-        )
-        when (readLine()?.toIntOrNull()) {
-            1 -> continue
-            2 -> return
-            else ->{ println("Нет такого варианта.")
-                return}
-
-        }
-    }
 }
 
 fun standardCommissionCalculator() {
-    if (amount == null || amount <= 0 || amount > 150_000) {
+    if (amount <= 0 || amount > 150_000) {
         println("Неправильная сумма.")
     } else {
         val commissionMirVisa = amount * STANDARD_RATE
         val finalCommission = if (commissionMirVisa < MINIMUM_RATE) MINIMUM_RATE else commissionMirVisa
-        println("$finalCommission руб. - комиссия с $cardChoice")
+        println("$finalCommission руб. - комиссия с $inputCardChoice")
     }
 }
